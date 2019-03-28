82d6741817741a1d57e656c53ab2d6052af25fd2
Kyle Monahan <kyle.monahan2000@gmail.com>
2019-03-28 15:46:21 -0500

    finished chart creation and destruction

--- js/cart.js
+++ js/cart.js
@@ -20,12 +20,13 @@ function renderCart() {
 
 // TODO: Remove all of the rows (tr) in the cart table (tbody)
 function clearCart() {
-
+  for(var i = 0; i < Cart.length; i++)
+    var destroy = document.getElementsByTagName('tfoot')[0].innerHTML='';
 }
 
 // TODO: Fill in the <tr>'s under the <tbody> for each item in the cart
 function showCart() {
-  var tableBody = document.getElementByTagName('tbody')[0];
+  var tableBody = document.getElementsByTagName('tbody')[0];
   for(var i = 0; i < Cart.length; i++){
     var tableRow = document.createElement('tr');
     var tableData = document.createElement('td');
@@ -44,9 +45,14 @@ function showCart() {
   // TODO: Add the TR to the TBODY and each of the TD's to the TR
 
 }
+function pickItemToRemove(){
+  document.getElementsByTagName('tr').addEventListener('click', removeItemFromCart);
+}
 
 function removeItemFromCart(event) {
-
+  Cart = cart.filter(pickItemToRemove);
+  localStorage.setItem('data', Cart)
+  renderChart();
   // TODO: When a delete link is clicked, rebuild the Cart array without that item
   // TODO: Save the cart back to local storage
   // TODO: Re-draw the cart table
