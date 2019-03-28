170f74f7e81c65a29ce34a6581dcdf47f6beb354
Kyle Monahan <kyle.monahan2000@gmail.com>
2019-03-28 16:44:50 -0500

    added function to remove items

--- js/cart.js
+++ js/cart.js
@@ -1,7 +1,6 @@
 'use strict';
 
 var Cart = [];
-
 // Create an event listener so that when the delete link is clicked, the removeItemFromCart 
 //method is invoked.
 var table = document.getElementById('cart');
@@ -9,6 +8,7 @@ table.addEventListener('click', removeItemFromCart);
 
 function loadCart() {
   Cart = JSON.parse(localStorage.getItem('cartData')) || [];
+  console.log(Cart);
 }
 
 // Make magic happen --- re-pull the Cart, clear out the screen and re-draw it
@@ -21,7 +21,7 @@ function renderCart() {
 // TODO: Remove all of the rows (tr) in the cart table (tbody)
 function clearCart() {
   for(var i = 0; i < Cart.length; i++)
-    var destroy = document.getElementsByTagName('tfoot')[0].innerHTML='';
+    var destroy = document.getElementsByTagName('tbody')[0].innerHTML='';
 }
 
 // TODO: Fill in the <tr>'s under the <tbody> for each item in the cart
@@ -29,13 +29,28 @@ function showCart() {
   var tableBody = document.getElementsByTagName('tbody')[0];
   for(var i = 0; i < Cart.length; i++){
     var tableRow = document.createElement('tr');
-    var tableData = document.createElement('td');
-    tableData.textContent = removeItemFromCart(Cart[i]);
-    tableData.textContent = Cart[i].item;
-    tabledata.textContent = Cart[i].quantity;
-    tableRow.appendChild(tableData);
+    var tableX = document.createElement('td');
+    var tableItem = document.createElement('td');
+    var tableQuantity = document.createElement('td');
+    
+    tableX.addEventListener('click', removeItemFromCart);
+    tableX.setAttribute('id', Cart[i].item);
+
+    tableX.textContent = 'X';
+    tableItem.textContent = Cart[i].item;
+    tableQuantity.textContent = Cart[i].quantity;
+
     tableBody.appendChild(tableRow);
+    
+    tableRow.appendChild(tableX);
+    tableRow.appendChild(tableQuantity);
+    tableRow.appendChild(tableItem);
+
+    console.log('hello');
   }
+  //
+  //tableData.textContent = Cart[i].quantity;
+
 
   // TODO: Find the table body
 
@@ -45,16 +60,12 @@ function showCart() {
   // TODO: Add the TR to the TBODY and each of the TD's to the TR
 
 }
-function pickItemToRemove(){
-  for(var i = 0; i < Cart.length; i++){
-  document.getElementsByTagName('tr')[i].addEventListener('click', removeItemFromCart);
-  }
-}
+
 
 function removeItemFromCart(event) {
-  Cart = Cart.filter(pickItemToRemove());
+  Cart = Cart.filter(product => product.item !== event.target.id);
   localStorage.setItem('cartData', JSON.stringify(Cart))
-  renderChart();
+  renderCart();
   // TODO: When a delete link is clicked, rebuild the Cart array without that item
   // TODO: Save the cart back to local storage
   // TODO: Re-draw the cart table
